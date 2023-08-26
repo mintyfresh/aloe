246dbac7128ec73ccc1af9878ecbab59a805d902
Minty Fresh <7896757+mintyfresh@users.noreply.github.com>
2023-08-26 02:12:50 -0400

    Demodulize custom IDs

--- app/lib/discord/components.rb
+++ app/lib/discord/components.rb
@@ -6,13 +6,17 @@ module Discord
     # @param record [ApplicationRecord, nil]
     # @return [String]
     def self.encode_custom_id(component, record = nil)
-      [component.class.name, record&.id].compact.join('/')
+      [component.class.name.demodulize, record&.id].compact.join('/')
     end
 
     # @param custom_id [String]
-    # @return [(Class<Discord::Components::Base>, Integer)]
+    # @return [(Class<Discord::Components::Base>, Integer), nil]
     def self.decode_custom_id(custom_id)
-      custom_id.split('/', 2)
+      component_class, record_id = custom_id.split('/', 2)
+      component_class = "Discord::Components::#{component_class}".safe_constantize
+      raise unless component_class.is_a?(Class) && component_class < Base
+
+      [component_class, record_id&.to_i]
     end
 
     # @param interaction [Hash]
@@ -22,10 +26,7 @@ module Discord
       return if custom_id.blank?
 
       component_class, record_id = decode_custom_id(custom_id)
-      return if component_class.blank?
-
-      component_class = component_class.safe_constantize
-      return unless component_class.is_a?(Class) && component_class < Base
+      return if component_class.nil?
 
       component_class.try(:respond_to_interaction, interaction, record_id)
     end
